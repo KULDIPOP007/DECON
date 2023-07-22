f0db9a6574f94a2992e20fccd763ff11139be094
KULDIPOP007 <140240449+KULDIPOP007@users.noreply.github.com>
2023-07-22 16:38:37 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -191,7 +191,7 @@ async def gen_qthumb(videoid, user_id):
         image1 = changeImageSize(1280, 720, youtube)
         sex = changeImageSize(1080, 420, youtube)
         image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(20))
+        background = image2.filter(filter=ImageFilter.BoxBlur(30))
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.6)
         logo = ImageOps.expand(sex, border=15, fill="white")
@@ -205,7 +205,7 @@ async def gen_qthumb(videoid, user_id):
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
-            (1065, 5), f"NOBITA MUSIC", fill="white", font=name_font
+            (1065, 5), f"XSTAR MUSIC", fill="white", font=name_font
         )
 
         draw.text(
